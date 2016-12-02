f56a91fb83cad46bf19fa142192644b38ea762ea
iSeeYourBalls <morozov.yura@icloud.com>
2016-12-02 21:02:29 +0200

    8-й урок

---
+++ lesson9/src/com/company/Calc.java
@@ -0,0 +1,63 @@
+package com.company;
+
+public class Calc {
+    public static int sum(int a,int b)
+    {
+       return a+b;
+    }
+
+    public static double sum(double a, double b)
+    {
+        return a+b;
+    }
+
+    public static int sum(String a, String b)
+    {
+        return Integer.parseInt(a) + Integer.parseInt(b);
+    }
+
+    public static int minus(int a,int b)
+    {
+        return a-b;
+    }
+
+    public static double minus(double a, double b)
+    {
+        return a-b;
+    }
+
+    public static int minus(String a, String b)
+    {
+        return Integer.parseInt(a) - Integer.parseInt(b);
+    }
+
+    public static int divided(int a,int b)
+    {
+        return a/b;
+    }
+
+    public static double divided(double a, double b)
+    {
+        return a/b;
+    }
+
+    public static int divided(String a, String b)
+    {
+        return Integer.parseInt(a) / Integer.parseInt(b);
+    }
+
+    public static int multiply(int a,int b)
+    {
+        return a*b;
+    }
+
+    public static double multiply(double a, double b)
+    {
+        return a*b;
+    }
+
+    public static int multiply(String a, String b)
+    {
+        return Integer.parseInt(a) * Integer.parseInt(b);
+    }
+}
---
+++ lesson9/src/com/company/Main.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        while (true)
+        {
+            System.out.print("Enter operator : ");
+
+            String operator = scanner.next();
+
+            System.out.print("Enter first operand : ");
+            int a = scanner.nextInt();
+
+            System.out.print("Enter second operand : ");
+            int b = scanner.nextInt();
+
+            switch (operator) {
+                case "+" :
+                    System.out.println(a + " " + operator + " " + b + " = " + Calc.sum(a, b));
+                    break;
+            }
+            System.out.println("What you like to continue? Enter y/n ");
+            
+            String exitWork = scanner.next();
+
+            if(exitWork != null && exitWork.length() > 0 && !exitWork.equalsIgnoreCase("y")) break;;
+        }
+    }
+}

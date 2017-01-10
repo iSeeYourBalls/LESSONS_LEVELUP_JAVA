c15769558ee2b51a24389826c3520551ee1620f7
java <java@training.workrocks.ua>
2017-01-10 20:05:47 +0200

    17

---
+++ lesson17/src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import com.company.view.impl.CreateCitizenDialog;
+
+public class Main {
+
+    public static void main(String[] args) {
+        CreateCitizenDialog createCitizenDialog = new CreateCitizenDialog();
+        createCitizenDialog.setVisible(true);
+    }
+}
